49461a80bc188b1391270cb48e13a80f1b3edb38
Hardelele <Hardelele@yahoo.com>
2020-03-27 12:14:28 +0300

    Add CreditParametersRepository as JpaRepository

---
+++ src/main/java/com/github/hardelele/cca/repositories/CreditParametersRepository.java
@@ -0,0 +1,11 @@
+package com.github.hardelele.cca.repositories;
+
+import com.github.hardelele.cca.models.CreditParameters;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CreditParametersRepository extends JpaRepository<CreditParameters, UUID> {
+}
