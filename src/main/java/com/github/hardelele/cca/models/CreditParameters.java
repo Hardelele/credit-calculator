bbbb829edfef69b5d4440e4bc11eec9e8eeeccce
Hardelele <Hardelele@yahoo.com>
2020-03-27 12:14:10 +0300

    Add CreditParameters entity

---
+++ src/main/java/com/github/hardelele/cca/models/CreditParameters.java
@@ -0,0 +1,22 @@
+package com.github.hardelele.cca.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "params")
+public class CreditParameters {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private double percent;
+}
