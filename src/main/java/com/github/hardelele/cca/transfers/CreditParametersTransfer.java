49f34dc2d0d96e884fd72664ac09cddca16fb737
Hardelele <Hardelele@yahoo.com>
2020-03-27 14:06:53 +0300

    Add CreditParametersTransfer

---
+++ src/main/java/com/github/hardelele/cca/transfers/CreditParametersTransfer.java
@@ -0,0 +1,13 @@
+package com.github.hardelele.cca.transfers;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreditParametersTransfer {
+
+    private double percent;
+}
