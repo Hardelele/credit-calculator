c704e0228b452de5fad0953aa14518acfdb925da
Hardelele <Hardelele@yahoo.com>
2020-03-27 12:15:17 +0300

    Add @SpringBootApplication and psvm SpringApplication

--- src/main/java/com/github/hardelele/cca/CreditApplication.java
+++ src/main/java/com/github/hardelele/cca/CreditApplication.java
@@ -1,4 +1,11 @@
 package com.github.hardelele.cca;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CreditApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CreditApplication.class, args);
+    }
 }
