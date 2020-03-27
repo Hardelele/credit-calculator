303db77fe66b6177221a0068fe089783d0fd9223
Hardelele <Hardelele@yahoo.com>
2020-03-27 19:00:49 +0300

    Create PayoutInfo

---
+++ src/main/java/com/github/hardelele/cca/models/transfers/PayoutInfo.java
@@ -0,0 +1,15 @@
+package com.github.hardelele.cca.models.transfers;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PayoutInfo {
+
+    private int number;
+    private double mainPayout;
+    private double percentagePart;
+}
